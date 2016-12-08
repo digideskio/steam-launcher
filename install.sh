5d32bc4b1523265b0eec22b60241b1a6cd108f86
Drako Frost <drakofrost@users.noreply.github.com>
2016-12-08 02:00:43 -0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-wget https://github.com/drakofrost/steam-launcher/blob/master/steam-launcher.sh
-wget https://github.com/drakofrost/steam-launcher/blob/master/steam-launcher.png
-wget https://github.com/drakofrost/steam-launcher/blob/master/steam-launcher.desktop
+wget https://raw.githubusercontent.com/drakofrost/steam-launcher/master/steam-launcher.sh
+wget https://github.com/drakofrost/steam-launcher/raw/master/steam-launcher.png
+wget https://raw.githubusercontent.com/drakofrost/steam-launcher/master/steam-launcher.desktop
 mkdir /opt/steam-launcher
 mv steam-launcher.sh steam-launcher.png /opt/steam-launcher
 mv steam-launcher.desktop /usr/share/applications/ 
