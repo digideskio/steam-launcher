74c99d6843ae80b2bb2f7c68fb449f2e3de87051
Drako Frost <drakofrost@users.noreply.github.com>
2016-12-08 02:24:10 -0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@
 wget https://raw.githubusercontent.com/drakofrost/steam-launcher/master/steam-launcher.sh
 wget https://github.com/drakofrost/steam-launcher/raw/master/steam-launcher.png
 wget https://raw.githubusercontent.com/drakofrost/steam-launcher/master/steam-launcher.desktop
+chmod +x steam-launcher.sh
 mkdir /opt/steam-launcher
 mv steam-launcher.sh steam-launcher.png /opt/steam-launcher
-mv steam-launcher.desktop /usr/share/applications/ 
+mv steam-launcher.desktop /usr/share/applications/
 echo Installation complete!
